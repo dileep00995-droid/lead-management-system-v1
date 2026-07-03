e4cc4efcdbcfe20e55106421fbfb9bc5a724573d
dileep00995-droid <dileep00995@gmail.com>
2026-07-03 15:02:56 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -16,7 +16,7 @@ const iti = window.intlTelInput(phoneInput, {
   initialCountry: "in",
   preferredCountries: ["in", "us", "gb"],
   separateDialCode: true,
-  strictMode: false,
+  strictMode: true,
   loadUtils: () =>
     import("https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.0/build/js/utils.js"),
 });
